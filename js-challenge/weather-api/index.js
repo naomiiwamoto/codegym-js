cd434ec2f72e1905a103a0da6a9618ad8f4ee2f1
naomikohara <naaaomi.0717@gmail.com>
2022-02-08 13:38:02 +0900

    js課題『天気API』修正版

--- js-challenge/weather-api/index.js
+++ js-challenge/weather-api/index.js
@@ -1,56 +1,32 @@
 
-function areaWhether() {
+const areaWeather = () => {
     //プルダウンの情報を受け取る
-    var city = document.getElementById("city").value;
-    var text = '';
-    if (city === 'London') {
-        currentWhether();
-    }
-    else if (city === 'Tokyo') {
-        currentWhether();
-    }
-    else if (city === 'Paris') {
-        currentWhether();
-    }
-    else if (city === 'New York') {
-        currentWhether();
-    }
-    else if (city === 'Indonesia') {
-        currentWhether();
+    const city = document.getElementById("city").value;
+    if (['London', 'Tokyo', 'Paris', 'New York', 'Indonesia'].includes(city)) {
+        currentWeather();
     }
 };
-
 //Ajaxで情報を取得
-function currentWhether() {
+const currentWeather = () => {
 
-    var city = document.getElementById("city").value;
-    var api_key = "4b5774e9f3d2a07b84f0f2f88e486224";
-    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key + "&lang=ja&units=metric";
-    var xhr = new XMLHttpRequest();
+    const city = document.getElementById("city").value;
+    const api_key = "4b5774e9f3d2a07b84f0f2f88e486224";
+    const url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key + "&lang=ja&units=metric";
+    const xhr = new XMLHttpRequest();
     xhr.open("GET", url);
     xhr.send();
 
-    xhr.onreadystatechange = function () {
+    xhr.onreadystatechange = () => {
 
         if (xhr.readyState === 4) {
 
             const obj = JSON.parse(xhr.responseText);
-            var whether = (obj.weather[0].description);//天気
-            var temperature = (obj.main.temp);//気温
-            var humidity = (obj.main.humidity);//湿度
-
-
-
-            document.getElementById("whether").innerText = whether;
+            const weather = (obj.weather[0].description);//天気
+            const temperature = (obj.main.temp);//気温
+            const humidity = (obj.main.humidity);//湿度
+            document.getElementById("weather").innerText = weather;
             document.getElementById("humidity").innerText = humidity;
             document.getElementById("temperature").innerText = temperature;
-
-
-
         }
     }
 }
-
-
-
-
